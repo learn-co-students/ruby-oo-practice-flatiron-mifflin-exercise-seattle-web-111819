7e0a5b29a753078b2b46aa9e0c713519e8000a6e
Alice Won <alicewon4@gmail.com>
2019-11-25 13:34:36 -0800

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,46 @@
 class Employee
+  attr_accessor :name, :salary, :manager
+  @@all = []
 
+  def initialize(name, salary, manager)
+    @name = name
+    @salary = salary
+    @manager = manager
+    @@all << self
+  end
+
+  def self.all
+    #    * returns an `Array` of all the employees
+    @@all
+  end
+
+  def self.paid_over(number)
+    #takes a `Fixnum` argument and returns an `Array` of all the employees whose salaries are over that amount
+    
+    @@all.select { |employee|employee.salary > number}
+  end
+
+  def self.find_by_department(departments)
+    #takes a `String` argument that is the name of a department and returns the first employee whose manager is working in that department
+@@all.find {|employee|employee.manager.department == departments}
+
+  end
+
+  def tax_bracket # e1.tax_bracket
+    #returns an `Array` of all the employees whose salaries are within $1000 (± 1000) of the employee who invoked the method
+  
+    max_value = self.salary + 1000
+    min_value = self.salary - 1000
+
+    tax_bracket = @@all.select {|employee| employee.salary >= min_value && employee.salary <= max_value}
+    
+    tax_bracket.delete(self.salary)
+    tax_bracket
+
+    #(employee.salary-1000).upto(employee.salary+1000)}
+    binding.pry
+    #(employee.salary+1000).upto(employee.salary-1000)}.to_i
+
+  end
+  
 end
--- run.rb
+++ run.rb
@@ -2,8 +2,36 @@ require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
 
-
 #Test your code here
+d1 = "Accounting"
+d2 = "Development"
+d3 = "Research"
+d4 = "Marketing"
+
+m1 = Manager.new("Cindy", d1, 30)
+m2 = Manager.new("Jen", d2, 40)
+m3 = Manager.new("Nicole", d3, 50)
+m4 = Manager.new("Alice", d4, 60)
+m5 = Manager.new("Soundarya", d1, 20)
+m6 = Manager.new("Natali", d2, 45)
+m7 = Manager.new("Amy", d3, 35)
+
+e1 = Employee.new("Bob", 100, m1)
+e2 = Employee.new("Josephine",200, m2)
+e3 = Employee.new("Mary", 300, m3)
+e4 = Employee.new("Joe", 400, m4)
+e5 = Employee.new("Santa", 500, m5)
+e6 = Employee.new("Avi", 600, m6)
+e7 = Employee.new("Kevin", 700, m7)
+e8 = Employee.new("Celine", 800, m7)
+e9 = Employee.new("Karl", 900, m7)
+e10 = Employee.new("Coco", 1000, m7)
+e11 = Employee.new("Lauren", 1100, m6)
+e12 = Employee.new("Ralph", 1200, m5)
+e13 = Employee.new("Carl", 3000, m4)
+e14 = Employee.new("Beyonce", 2000, m3)
+
+
 
 
 binding.pry

1eb1b7c8fab450175d19e9761aa03223390e348f
github username <github email address>
2019-11-25 10:49:20 -0800

    finally, donegit add . :]

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,41 @@
 class Manager
 
+    attr_accessor :name, :department, :age
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name 
+        @department = department
+        @age = age
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+#   return array of all employees of that manager
+#   manager --> [employees]
+    def employees
+        Employee.all.select { |employee| employee.manager == self}
+    end 
+
+#   takes in employee name & salary, 
+#   adds that employee to list of employees for that manager
+    def hire_employee(employee_name, salary)
+        Employee.new(employee_name, salary, self)
+    end 
+
+#   returns [] of all dep name that every mang oversees
+    def self.all_departments
+        @@all.map { |manager| manager.department}
+    end 
+
+#   return average age of all managers (float)
+    def self.average_age
+        ages = @@all.map { |manager| manager.age}
+        ages.sum/ages.length
+    end 
+
 end
--- run.rb
+++ run.rb
@@ -2,8 +2,20 @@ require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
 
+man1 = Manager.new("Alice", "Department A", 35)
+man2 = Manager.new("Bob", "Department B", 44)
 
-#Test your code here
+emp1 = Employee.new("Caleb", 45000, man1)
+emp2 = Employee.new("David", 55000, man1)
+emp3 = Employee.new("Ella", 44999, man2)
+emp4 = Employee.new("Fiona", 45500, man2)
+emp5 = Employee.new("Gabe", 60000, man2)
+
+
+
+
+
+#Test your code heres
 
 
 binding.pry
